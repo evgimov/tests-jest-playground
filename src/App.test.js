8b40c9f47bbc84c6fb4572c48abc8fc703914e35
evgimov <evgimov@gmail.com>
2018-12-18 22:02:39 -0800

    Created first snapshot test with jest

---
+++ src/App.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import App, {Button} from './App';
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create (
+      <App />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  
+});
+
+describe('Button', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>Test</Button>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Button>Test</Button>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
